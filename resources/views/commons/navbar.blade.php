fe44ee15888ed6dd10f934cdb77ec79dce5dc2fc
tomokikojima <tomoki-kojima@dac.co.jp>
2020-06-01 05:48:12 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class = "mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a class="navbar-brand" href="/">Microposts</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-bar" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <div class="collapse navbar-collapse" id="nav-bar">
+                <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</header>
